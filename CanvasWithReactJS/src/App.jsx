8b9d40db9d05ce5a770bd5826192d30b011cad38
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-02-17 18:07:27 +0700

    Tạo dự án với React

---
+++ CanvasWithReactJS/src/App.jsx
@@ -0,0 +1,9 @@
+import AnimatedCircle from "./pages/AnimatedCircle/AnimatedCircle";
+
+export default function App() {
+  return (
+    <div className="relative w-screen h-screen bg-black">
+      <AnimatedCircle />
+    </div>
+  );
+}
