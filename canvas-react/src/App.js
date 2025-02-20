2d914a29e6a730c324fb6b6eacbd4cc10d745929
phucnh204 <137475114+phucnh204@users.noreply.github.com>
2025-02-20 17:01:51 +0700

    Cập nhật Ui file app.js

--- canvas-react/src/App.js
+++ canvas-react/src/App.js
@@ -1,14 +1,35 @@
-
 import "./App.css";
-import AnimatedCircle from './pages/AnimatedCircle';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import AnimatedCircle from "./pages/AnimatedCircle";
 import ParallelogramCanvas from "./pages/ParallelogramCanvas";
 
-
 function App() {
-  return <div className="App">
-    {/* <AnimatedCircle/> */}
-    {/* <ParallelogramCanvas/> */}
-  </div>;
+  return (
+    <Router>
+      <nav style={{ padding: "10px", background: "#333", color: "#fff" }}>
+        <Link
+          to="/animated-circle"
+          style={{ marginRight: "10px", color: "white" }}
+        >
+          Animated Circle
+        </Link>
+        <Link to="/parallelogram" style={{ color: "white" }}>
+          Parallelogram
+        </Link>
+      </nav>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <h1 style={{ textAlign: "center" }}>Welcome to Shape Animations</h1>
+          }
+        />
+        <Route path="/animated-circle" element={<AnimatedCircle />} />
+        <Route path="/parallelogram" element={<ParallelogramCanvas />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
